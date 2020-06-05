9a73b1e02d77c98a73cf46d94ae57c0c84cc199a
khsachan <khsachan@in.ibm.com>
2020-06-05 15:24:34 +0530

    Layout component created

---
+++ burger-builder/src/components/Layout/Layout.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Aux from "../../hoc/Auxilliary";
+
+const layout = props => (
+  <Aux>
+    <div>Toolbar, Sidedrawr, Backdrop</div>
+    <main>{props.children}</main>
+  </Aux>
+);
+
+export default layout;
